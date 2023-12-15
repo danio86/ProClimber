f50cf112a6fbfcf722d846a2c314eea4ad1d2474
danio86 <d.harder@gmx.at>
2023-12-15 15:40:08 +0000

    Move project to root folder

---
+++ backendOLD/base/tests.py
@@ -0,0 +1,194 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.urls import reverse
+from .models import Product, Order, OrderItem, ShippingAddress
+from .serializers import ProductSerializer
+
+User = get_user_model()
+
+
+
+class ProductViewTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        # self.user = User.objects.create_user(username='testuser', password='testpass', is_superuser=True)
+        self.user = User.objects.create_user(username='testuser', password='testpass', is_superuser=True, is_staff=True)
+        self.product = Product.objects.create(
+            user=self.user,
+            name='Test Product',
+            price=10,
+            brand='Test Brand',
+            countInStock=10,
+            category='Test Category',
+            description='Test Description'
+        )
+
+    def test_get_product(self):
+        response = self.client.get(reverse('products:product', kwargs={'pk': self.product._id}))
+        product = Product.objects.get(_id=self.product._id)
+        serializer = ProductSerializer(product, many=False)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_product(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(reverse('products:product-delete', kwargs={'pk': self.product._id}))
+        self.assertEqual(response.status_code, 200)
+        # Check if the product still exists in the database
+        product_exists = Product.objects.filter(_id=self.product._id).exists()
+        self.assertFalse(product_exists)
+
+    def test_update_product(self):
+        self.client.force_authenticate(user=self.user)
+        new_product_details = {
+            'name': 'Updated Product',
+            'price': 30,
+            'brand': 'Updated Brand',
+            'countInStock': 30,
+            'category': 'Updated Category',
+            'description': 'Updated Description'
+        }
+        response = self.client.put(reverse('products:product-update', kwargs={'pk': self.product._id}), new_product_details, format='json')
+        self.assertEqual(response.status_code, 200)
+        # Check if the product details have been updated correctly
+        updated_product = Product.objects.get(_id=self.product._id)
+        for field, value in new_product_details.items():
+            self.assertEqual(getattr(updated_product, field), value)
+
+
+
+
+class UserViewTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='adam@example.com', password='pass', first_name='Adam', is_superuser=True, is_staff=True)
+        self.admin = User.objects.create_user(username='admin@example.com', password='pass', first_name='Admin', is_staff=True, is_superuser=True)
+        self.product = Product.objects.create(
+            user=self.user,
+            name='Test Product',
+            price=10,
+            brand='Test Brand',
+            countInStock=10,
+            category='Test Category',
+            description='Test Description'
+        )
+        self.order = Order.objects.create(
+            user=self.user,
+            paymentMethod='Test Payment Method',
+            taxPrice=1.0,
+            shippingPrice=1.0,
+            totalPrice=12.0,
+        )
+        self.orderItem = OrderItem.objects.create(
+            product=self.product,
+            order=self.order,
+            name=self.product.name,
+            qty=1,
+            price=self.product.price,
+            image=self.product.image.url,
+        )
+        self.shippingAddress = ShippingAddress.objects.create(
+            order=self.order,
+            address='Test Address',
+            city='Test City',
+            postalCode='Test Postal Code',
+            country='Test Country',
+        )
+
+    def test_can_add_order_items(self):
+        self.client.force_authenticate(user=self.user)
+        # response = self.client.post('/orders/add/', {
+        response = self.client.post(reverse('orders-add'), {
+            'orderItems': [{
+                'product': str(self.product._id),
+                'qty': 1,
+                'price': self.product.price,
+            }],
+            'paymentMethod': 'Test Payment Method',
+            'taxPrice': 1.0,
+            'shippingPrice': 1.0,
+            'totalPrice': 12.0,
+            'shippingAddress': {
+                'address': 'Test Address',
+                'city': 'Test City',
+                'postalCode': 'Test Postal Code',
+                'country': 'Test Country',
+            },
+        }, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_delete_user(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.delete(reverse('user-delete', kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_get_user_by_id(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.get(reverse('user', kwargs={'pk': self.user.pk}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+
+
+class OrderViewTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='adam', email='adam@example.com', password='pass', is_superuser=True, is_staff=True)
+        self.admin = User.objects.create_user(username='admin@example.com', password='pass', first_name='Admin', is_staff=True)
+
+    def test_can_register_user(self):
+        response = self.client.post(reverse('register'), {
+            'name': 'Test User',
+            'email': 'test@example.com',
+            'password': 'testpass',
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], 'test@example.com')
+
+    def test_can_update_user_profile(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(reverse('users-profile-update'), {
+            'name': 'Updated User',
+            'email': 'updated@example.com',
+            'password': 'newpass',
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], 'updated@example.com')
+
+    def test_can_get_user_profile(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(reverse('users-profile'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], 'adam@example.com')
+
+    def test_can_get_users(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.get(reverse('users'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_can_get_user_by_id(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.get(reverse('user', args=[self.user.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], 'adam@example.com')
+
+    def test_can_update_user(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.put(reverse('user-update', args=[self.user.id]), {
+            'name': 'Updated User',
+            'email': 'updated@example.com',
+            'isAdmin': False,
+        })
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], 'updated@example.com')
+
+    def test_can_delete_user(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.delete(reverse('user-delete', args=[self.user.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
---
+++ backendOLD/base/views/product_views.py
@@ -0,0 +1,135 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, IsAdminUser #IsAuthenticated is for logged in users
+from base.models import Product, Review
+from base.serializers import ProductSerializer
+from rest_framework import status
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+# @api_view(['GET'])
+# def getProducts(request):
+#     products = Product.objects.all()
+#     serializer = ProductSerializer(products, many=True) #many=True means we are serializing multiple products
+#     return Response(serializer.data)
+
+@api_view(['GET'])
+def getProducts(request):
+    query = request.query_params.get('keyword')
+    if query == None:
+        query = ''
+    products = Product.objects.filter(name__icontains=query) #icontains means case insensitive
+
+    # page = request.query_params.get('page')
+    # paginator = Paginator(products, 10)
+
+    # try:
+    #     products = paginator.page(page)
+    # except PageNotAnInteger:
+    #     products = paginator.page(1)
+    # except EmptyPage: #if page number is greater than total pages
+    #     products = paginator.page(paginator.num_pages)
+
+    # if page == None:
+    #     page = 1
+
+
+    serializer = ProductSerializer(products, many=True) #many=True means we are serializing multiple products
+    return Response(serializer.data) #paginator.num_pages gives total number of pages
+    # return Response({'products': serializer.data, 'page': page, 'pages': paginator.num_pages}) #paginator.num_pages gives total number of pages
+
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)#many=False means we are serializing a single product
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    user = request.user
+    product = Product.objects.create(
+        user=user,
+        name='Select Name',
+        price=0,
+        brand='Select Brand',
+        countInStock=0,
+        category='Select Category',
+        description=''
+    )
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+    product.name = data['name']
+    product.price = data['price']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.category = data['category']
+    product.description = data['description']
+    product.save()
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    productForDeletion = Product.objects.get(_id=pk)
+    productForDeletion.delete()
+    return Response('Product Deleted')
+
+
+@api_view(['POST'])
+def uploadImage(request):
+    data = request.data
+    product_id = data['product_id']
+    product = Product.objects.get(_id=product_id)
+    product.image = request.FILES.get('image')
+    product.save()
+    return Response('Image uploaded')
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id=pk)
+    data = request.data
+    #1 - Review of customer already exists
+    alreadyExists = product.review_set.filter(user=user).exists()
+    if alreadyExists:
+        content = {'detail': 'You have already reviewed this product'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    #2 - Custommer Review without Rating or 0
+    elif data['rating'] == 0:
+        content = {'detail': 'Please select a rating'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    #3 - Create new Review
+    else:
+        review = Review.objects.create(
+            user=user,
+            product=product,
+            name=user.first_name,
+            rating=data['rating'],
+            comment=data['comment']
+        )
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+        total = 0
+        for i in reviews:
+            total += i.rating
+        product.rating = total / len(reviews)
+        product.save()
+        return Response('Review Added')
