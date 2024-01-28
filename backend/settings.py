7039bec1155f77feef56cf3df9d2c1329ebfb918
danio86 <d.harder@gmx.at>
2024-01-28 16:51:07 +0000

    set debug to false

--- backend/settings.py
+++ backend/settings.py
@@ -56,7 +56,7 @@ if 'DEV' not in os.environ:
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = 'DEV' in os.environ
 
 
